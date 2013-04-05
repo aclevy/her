148ef2d570e50174011380d2a9b8bdb7fe4f40da
Alex Levy <alex.levy@alumni.tufts.edu>
2013-04-05 09:36:33 -0400

    Move default method actions into model.rb

--- lib/her/model.rb
+++ lib/her/model.rb
@@ -47,6 +47,8 @@ module Her
       collection_path "#{base_path}"
       resource_path "#{base_path}/:id"
       uses_api Her::API.default_api
+      method_for :create, :post
+      method_for :update, :put
     end
 
     # Returns true if attribute_name is
